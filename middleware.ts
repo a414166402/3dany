2576ae1e68f1318cff3813c336034aff64f863e3
a414166402 <414166402@qq.com>
2024-05-08 10:54:54 +0800

    back old clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,31 @@
+// import { NextResponse } from "next/server";
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware({
+//   publicRoutes: ["/", "/pricing", "/api/get-user-info"],
+
+//   afterAuth(auth, req, evt) {
+//     if (!auth.userId && !auth.isPublicRoute) {
+//       if (auth.isApiRoute) {
+//         return NextResponse.json(
+//           { code: -2, message: "no auth" },
+//           { status: 401 }
+//         );
+//       } else {
+//         return NextResponse.redirect(new URL("/sign-in", req.url));
+//       }
+//     }
+
+//     return NextResponse.next();
+//   },
+// });
+
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { authMiddleware } from "@clerk/nextjs";
+import type { NextRequest } from 'next/server'
 
-export default authMiddleware({
-  publicRoutes: ["/", "/pricing", "/api/get-user-info"],
-
-  afterAuth(auth, req, evt) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      // if (auth.isApiRoute) {
-      //   return NextResponse.json(
-      //     { code: -2, message: "no auth" },
-      //     { status: 401 }
-      //   );
-      // } else {
-        return NextResponse.redirect(new URL("/sign-in", req.url));
-      // }
-    }
-
-    return NextResponse.next();
-  },
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
 });
 
 export const config = {
