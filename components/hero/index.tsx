8550e9f84fd2e6f99b8be1bd8d7c55a9354624a4
a414166402 <414166402@qq.com>
2024-05-09 11:52:52 +0800

    Optimize text rendering

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -4,7 +4,7 @@ export default function () {
       <h1 className="text-6xl font-bold mt-8 md:mt-24">
         <span className="text-primary"> {process.env.NEXT_PUBLIC_WEB_TITLE}</span>
       </h1>
-      <h2 className="text-4xl my-8 text-secondary-foreground">
+      <h2 className="text-4xl my-8">
         {process.env.NEXT_PUBLIC_WEB_DESCRIPTION}
       </h2>
     </section>
